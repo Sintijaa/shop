6829e0cd5773ffc463e12be59d6cfa42f5b6c5a7
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2024-02-25 22:39:37 +0200

    .

--- coffeeshop/src/App.jsx
+++ coffeeshop/src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
 import Header from './components/Header.jsx';
 import coffeeImage from './assets/images/775.png';
+import coffeeRing from './assets/images/776.png';
+import hotCoffeeImage from './assets/images/777.png';
 import wave1 from './assets/waves/wave1.png';
+import wave2 from './assets/waves/wave2.png';
 
 function App() {
 
@@ -19,7 +22,13 @@ function App() {
           </div>
         </div>
         <div className='secondPage'>
+        <img src={coffeeRing}  className='coffeeRing'/>
           <h1 className='h1_2'>Black Coffees</h1>
+        </div>
+        <img src={wave2}  className='wave2'/>
+        <div classname='thirdPage'>
+        <img src={hotCoffeeImage}  className='hotCoffeeImage'/>
+
         </div>
     </>
   );
