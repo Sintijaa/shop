3534b78caf18f33ddd470319114dfc582d3a8cde
Sintijaa <ipb21.s.martinsone@vtdt.edu.lv>
2024-02-27 13:14:13 +0200

    .

--- coffeeshop/src/App.jsx
+++ coffeeshop/src/App.jsx
@@ -3,8 +3,16 @@ import Header from './components/Header.jsx';
 import coffeeImage from './assets/images/775.png';
 import coffeeRing from './assets/images/776.png';
 import hotCoffeeImage from './assets/images/777.png';
+import whiteCoffeeImage from './assets/images/777.png';
+import voucherImage from './assets/images/voucher.png';
+import ellaImage from './assets/images/ella.png';
+import johnImage from './assets/images/john.png';
 import wave1 from './assets/waves/wave1.png';
 import wave2 from './assets/waves/wave2.png';
+import wave3 from './assets/waves/wave3.png';
+import wave4 from './assets/waves/wave4.png';
+
+
 
 function App() {
 
@@ -23,14 +31,63 @@ function App() {
         </div>
         <div className='secondPage'>
         <img src={coffeeRing}  className='coffeeRing'/>
-          <h1 className='h1_2'>Black Coffees</h1>
-          <button className='orderButton'>Order Now</button>
+          <div className='box'>
+            <h1 className='h1_2'>Black Coffees</h1>
+            <button className='ordersButton'>Order Now</button>
+          </div>
+          <div className='box'>
+            <p className='p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          </div>
         </div>
+
         <img src={wave2}  className='wave2'/>
-        <div classname='thirdPage'>
-        <img src={hotCoffeeImage}  className='hotCoffeeImage'/>
-        
-       
+        <div className='thirdPage'>
+          <img src={hotCoffeeImage}  className='hotCoffeeImage'/>
+          <div className='secondBox'>
+            <p className='p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          </div>
+          <div className='secondBox'>
+              <h1 className='h1_2'>Hot Coffees</h1>
+              <button className='ordersButtons'>Order Now</button>
+          </div>
+        </div>
+
+        <div className='fourthPage'>
+        <img src={whiteCoffeeImage}  className='whiteCoffeeImage'/>
+          <div className='thirdBox1'>
+            <h1 className='h1_2'>White coffe</h1>
+            <button className='OrdersButtons'>Order Now</button>
+          </div>
+          <div className='thirdBox'>
+          <p className='p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+          </div>
+        </div>
+
+        <div className='fifthPage'>
+          <h1 className='h1_2'>Voucher</h1>
+          <img src={voucherImage}  className='voucherImage'/>
+          <button className='getButton'>Get Voucher Now</button>
+        </div>
+
+
+        <div className='reviewsPage'>
+          <h1 className='h1_2'>Customer Reviews</h1>
+            <div className='reviewsBox'>
+              <div className='ellaReviewsBox'>
+                <img src={johnImage}  className='johnImage'/>
+                <p className='p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+               </div>
+                <div className='jonhReviewsBox'>
+                  <img src={ellaImage}  className='ellaImage'/>
+                  <p className='p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                </div>
+              </div>
+            <img src={wave3}  className='wave3'/>
+        </div>
+
+        <div className='openPage'>
+          <h1 className='h1_2'>We are open</h1>
+
         </div>
     </>
   );
